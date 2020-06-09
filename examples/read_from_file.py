cd63a56d7f3a91f7f46844f46bb9cc7a7f4c7681
chai <chaihegde56@gmail.com>
2020-06-09 16:49:42 +0530

    Read from different types of files and create dataframe

---
+++ examples/read_from_file.py
@@ -0,0 +1,53 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+
+
+def read_parquet(sqlcontext):
+    """
+    Function to read parquet file and return dataframe
+    """
+    dataframe = sqlcontext.read.parquet('/home/chai/students.parquet')
+    return dataframe
+
+
+def read_csv(sqlcontext):
+    """
+    Function to read csv file and return dataframe
+    """
+    dataframe = sqlcontext.read.csv('/home/chai/students.csv', header='true')
+    return dataframe
+
+
+def read_txt(sqlcontext):
+    """
+    Function to read csv file and return dataframe
+    """
+    dataframe = sqlcontext.read.text('/home/chai/test.text')
+    return dataframe
+
+
+def main():
+    spark = SparkSession.builder.\
+        master('local').appName('test').\
+        config('spark.executor.memory', '1gb').\
+        config('spark.cores.max', '2').\
+        getOrCreate()
+    sc = spark.sparkContext
+    sqlcontext = SQLContext(sc)
+
+    # Read parquet file
+    dataframe = read_parquet(sqlcontext)
+    dataframe.show()
+
+    # Read csv file
+    dataframe = read_csv(sqlcontext)
+    dataframe.show()
+
+    # Read text file
+    dataframe = read_txt(sqlcontext)
+    dataframe.show()
+
+
+if __name__ == '__main__':
+    main()
+
